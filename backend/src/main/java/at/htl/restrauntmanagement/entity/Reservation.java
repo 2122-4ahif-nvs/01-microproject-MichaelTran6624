eac32875e95b3e3e2960c961c7b5d2d968e46ece
MichaelTran6624 <michael.tran6624@gmail.com>
2022-01-03 11:52:14 +0100

    changed Table and Reservation into Entities

--- backend/src/main/java/at/htl/restrauntmanagement/entity/Reservation.java
+++ backend/src/main/java/at/htl/restrauntmanagement/entity/Reservation.java
@@ -1,4 +1,71 @@
 package at.htl.restrauntmanagement.entity;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    LocalDate reservationDate;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    Customer customer;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    Table table;
+
+    //region constructors
+    public Reservation() {
+    }
+
+    public Reservation(LocalDate reservationDate, Customer customer, Table table) {
+        this.reservationDate = reservationDate;
+        this.customer = customer;
+        this.table = table;
+    }
+//endregion
+
+    //region getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Table getTable() {
+        return table;
+    }
+
+    public void setTable(Table table) {
+        this.table = table;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return String.format("%d: %t %s %s", id, reservationDate, customer, table);
+    }
 }
--- backend/src/main/java/at/htl/restrauntmanagement/entity/Table.java
+++ backend/src/main/java/at/htl/restrauntmanagement/entity/Table.java
@@ -1,4 +1,60 @@
 package at.htl.restrauntmanagement.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Table {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    Long tableNr;
+
+    int seats;
+
+    //region constructors
+    public Table() {
+    }
+
+    public Table(Long tableNr, int seats) {
+        this.tableNr = tableNr;
+        this.seats = seats;
+    }
+    //endregion
+
+    //region getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTableNr() {
+        return tableNr;
+    }
+
+    public void setTableNr(Long tableNr) {
+        this.tableNr = tableNr;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+    //endregion
+
+
+    @Override
+    public String toString() {
+        return String.format("%d: TableNr: %d Seats: %d", id, tableNr, seats);
+    }
 }
