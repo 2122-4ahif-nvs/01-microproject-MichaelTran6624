58d558670b22af6f6be63ba2ff1f123e2703ec8a
MichaelTran6624 <michael.tran6624@gmail.com>
2022-01-03 11:51:18 +0100

    modified toString() method of Customer-Entity

--- backend/src/main/java/at/htl/restrauntmanagement/entity/Customer.java
+++ backend/src/main/java/at/htl/restrauntmanagement/entity/Customer.java
@@ -51,10 +51,7 @@ public class Customer {
 
     @Override
     public String toString() {
-        return "Customer{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
+        return String.format("%d: %s %s", id, firstName, lastName);
     }
 
 }
