3b6b04aa1a4580f02816fdcb89c55fa9f91d8cad
MichaelTran6624 <michael.tran6624@gmail.com>
2022-01-04 13:40:18 +0100

    added new Tests

--- backend/src/test/java/at/htl/restrauntmanagement/boundary/CustomerResourceTest.java
+++ backend/src/test/java/at/htl/restrauntmanagement/boundary/CustomerResourceTest.java
@@ -1,7 +1,11 @@
 package at.htl.restrauntmanagement.boundary;
 
+import at.htl.restrauntmanagement.entity.Customer;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
+
+import javax.validation.Valid;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.startsWith;
@@ -13,9 +17,27 @@ class CustomerResourceTest {
     @Test
     void getAllCustomers(){
         when()
-                .get("customer/all")
+                .get("customer/allCustomer")
                 .then()
                 .statusCode(200)
                 .body(startsWith("[{"));
+    }
+
+    @Test
+    void getAllTables() {
+        when()
+                .get("customer/allTables")
+                .then()
+                .statusCode(200)
+                .body(startsWith("{["));
+    }
+
+    @Test
+    void getAllReservations() {
+        when()
+                .get("customer/allReservations")
+                .then()
+                .statusCode(200)
+                .body(startsWith("{["));
     }
 }
