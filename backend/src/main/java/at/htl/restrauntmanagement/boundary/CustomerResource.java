f3a8af867385bdfcfc7c0746a0c4479e46d06c0f
MichaelTran6624 <michael.tran6624@gmail.com>
2022-01-04 11:34:39 +0100

    modified CustomerResource.java

--- backend/src/main/java/at/htl/restrauntmanagement/boundary/CustomerResource.java
+++ backend/src/main/java/at/htl/restrauntmanagement/boundary/CustomerResource.java
@@ -4,28 +4,15 @@ import at.htl.restrauntmanagement.control.CustomerRepository;
 import at.htl.restrauntmanagement.entity.Customer;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Query;
-import org.jboss.logging.Logger;
 
-import java.net.URI;
 import java.util.List;
 
 @GraphQLApi
-@Path("customer")
 public class CustomerResource {
-
-    @Inject
-    Logger logger;
 
     @Inject
     CustomerRepository customerRepository;
@@ -35,9 +22,7 @@ public class CustomerResource {
         return customerRepository.saveCustomer(customer);
     }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Query
+    @Query("allCustomer")
     public List<Customer> getAllCustomer() {
         return customerRepository.getAllCustomer();
     }
